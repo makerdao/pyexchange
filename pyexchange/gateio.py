1f873573f0cf4540a3aeaf6f4331ffd1df490415
reverendus <reverendus@invalid>
2018-01-20 17:52:12 +0100

    Retrieving and parsing the orders list

--- pyexchange/gateio.py
+++ pyexchange/gateio.py
@@ -19,10 +19,67 @@ import hashlib
 import logging
 import urllib
 import hmac
+from pprint import pformat
+from typing import List
 
 import requests
 
 from pymaker.numeric import Wad
+
+
+class Order:
+    def __init__(self, order_id: int, timestamp: int, pair: str,
+                 is_sell: bool, price: Wad, amount: Wad, amount_symbol: str,
+                 money: Wad, money_symbol: str, initial_amount: Wad, filled_amount: Wad):
+        assert(isinstance(order_id, int))
+        assert(isinstance(timestamp, int))
+        assert(isinstance(pair, str))
+        assert(isinstance(is_sell, bool))
+        assert(isinstance(price, Wad))
+        assert(isinstance(amount, Wad))
+        assert(isinstance(amount_symbol, str))
+        assert(isinstance(money, Wad))
+        assert(isinstance(money_symbol, str))
+        assert(isinstance(initial_amount, Wad))
+        assert(isinstance(filled_amount, Wad))
+
+        self.order_id = order_id
+        self.timestamp = timestamp
+        self.pair = pair
+        self.is_sell = is_sell
+        self.price = price
+        self.amount = amount
+        self.amount_symbol = amount_symbol
+        self.money = money
+        self.money_symbol = money_symbol
+        self.initial_amount = initial_amount
+        self.filled_amount = filled_amount
+
+    @property
+    def sell_to_buy_price(self) -> Wad:
+        return self.price
+
+    @property
+    def buy_to_sell_price(self) -> Wad:
+        return self.price
+
+    @property
+    def remaining_sell_amount(self) -> Wad:
+        #TODO
+        pass
+        #return (self.amount - self.deal_amount) if self.is_sell else (self.amount - self.deal_amount)*self.price
+
+    def __eq__(self, other):
+        assert(isinstance(other, Order))
+
+        return self.order_id == other.order_id and \
+               self.pair == other.pair
+
+    def __hash__(self):
+        return hash((self.order_id, self.pair))
+
+    def __repr__(self):
+        return pformat(vars(self))
 
 
 class GateIOApi:
@@ -64,9 +121,19 @@ class GateIOApi:
     def get_balances(self):
         return self._http_post("/api2/1/private/balances", {})
 
-    def get_orders(self):
-        #TODO add order parsing
-        return self._http_post("/api2/1/private/openOrders", {})
+    def get_orders(self) -> List[Order]:
+        result = self._http_post("/api2/1/private/openOrders", {})['orders']
+        return list(map(lambda item: Order(order_id=int(item['orderNumber']),
+                                           timestamp=int(item['timestamp']),
+                                           pair=item['currencyPair'],
+                                           is_sell=item['type'] == 'sell',
+                                           price=Wad.from_number(item['rate']),
+                                           amount=Wad.from_number(item['amount']),
+                                           amount_symbol=item['currencyPair'].split('_')[0],
+                                           money=Wad.from_number(item['total']),
+                                           money_symbol=item['currencyPair'].split('_')[1],
+                                           initial_amount=Wad.from_number(item['initialAmount']),
+                                           filled_amount=Wad.from_number(item['filledAmount'])), result))
 
     def get_order(self, pair: str, order_id: int):
         assert(isinstance(pair, str))
