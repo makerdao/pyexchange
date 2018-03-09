0cb322be825618c7cda158570a4d559e334a60c0
reverendus <reverendus@invalid>
2018-03-09 18:36:29 -0300

    Bittrex `get_all_trades()` API call implemented

---
+++ pyexchange/bittrex.py
@@ -0,0 +1,118 @@
+# This file is part of Maker Keeper Framework.
+#
+# Copyright (C) 2017-2018 reverendus
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import logging
+from pprint import pformat
+from typing import List
+
+import dateutil.parser
+import requests
+
+from pymaker.numeric import Wad
+
+
+class Trade:
+    def __init__(self,
+                 trade_id: id,
+                 timestamp: int,
+                 is_sell: bool,
+                 price: Wad,
+                 amount: Wad,
+                 money: Wad):
+        assert(isinstance(trade_id, int))
+        assert(isinstance(timestamp, int))
+        assert(isinstance(is_sell, bool))
+        assert(isinstance(price, Wad))
+        assert(isinstance(amount, Wad))
+        assert(isinstance(money, Wad))
+
+        self.trade_id = trade_id
+        self.timestamp = timestamp
+        self.is_sell = is_sell
+        self.price = price
+        self.amount = amount
+        self.money = money
+
+    def __eq__(self, other):
+        assert(isinstance(other, Trade))
+        return self.trade_id == other.trade_id and \
+               self.timestamp == other.timestamp and \
+               self.is_sell == other.is_sell and \
+               self.price == other.price and \
+               self.amount == other.amount and \
+               self.money == other.money
+
+    def __hash__(self):
+        return hash((self.trade_id,
+                     self.timestamp,
+                     self.is_sell,
+                     self.price,
+                     self.amount,
+                     self.money))
+
+    def __repr__(self):
+        return pformat(vars(self))
+
+
+class BittrexApi:
+    """Bittrex API interface.
+
+    Developed according to the following manual:
+    <https://bittrex.com/home/api>.
+    """
+
+    logger = logging.getLogger()
+
+    def __init__(self, api_server: str, timeout: float):
+        assert(isinstance(api_server, str))
+        assert(isinstance(timeout, float))
+
+        self.api_server = api_server
+        self.timeout = timeout
+
+    def get_all_trades(self, pair: str) -> List[Trade]:
+        assert(isinstance(pair, str))
+
+        result = self._http_get("/api/v1.1/public/getmarkethistory", f"market={pair}")["result"]
+        return list(map(lambda item: Trade(trade_id=int(item['Id']),
+                                           timestamp=int(dateutil.parser.parse(item['TimeStamp'] + 'Z').timestamp()),
+                                           is_sell=item['OrderType'] == 'SELL',
+                                           price=Wad.from_number(item['Price']),
+                                           amount=Wad.from_number(item['Quantity']),
+                                           money=Wad.from_number(item['Total'])), result))
+
+    @staticmethod
+    def _result(result) -> dict:
+        if not result.ok:
+            raise Exception(f"Bittrex API invalid HTTP response: {result.status_code} {result.reason}")
+
+        try:
+            data = result.json()
+        except Exception:
+            raise Exception(f"Bittrex API invalid JSON response: {result.text}")
+
+        if 'success' not in data or data['success'] is not True:
+            raise Exception(f"Negative Bittrex response: {data}")
+
+        return data
+
+    def _http_get(self, resource: str, params: str):
+        assert(isinstance(resource, str))
+        assert(isinstance(params, str))
+
+        return self._result(requests.get(url=f"{self.api_server}{resource}?{params}",
+                                         timeout=self.timeout))
