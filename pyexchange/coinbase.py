aba0bc7aaf9672bc63c67b7f16bbc8664bfd4ab7
grandizzy <grandizzy@localhost>
2018-12-19 21:09:31 +0200

    Add Coinbase support

---
+++ pyexchange/coinbase.py
@@ -0,0 +1,295 @@
+# This file is part of Maker Keeper Framework.
+#
+# Copyright (C) 2018 grandizzy
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import logging
+from pprint import pformat
+from pyexchange.api import PyexAPI
+import hmac
+import hashlib
+import time
+import base64
+import requests
+import json
+import dateutil.parser
+from pymaker import Wad
+from typing import Optional, List
+
+
+class Order:
+    def __init__(self,
+                 order_id: int,
+                 pair: str,
+                 is_sell: bool,
+                 price: Wad,
+                 amount: Wad):
+
+        assert(isinstance(pair, str))
+        assert(isinstance(is_sell, bool))
+        assert(isinstance(price, Wad))
+        assert(isinstance(amount, Wad))
+
+        self.order_id = order_id
+        self.pair = pair
+        self.is_sell = is_sell
+        self.price = price
+        self.amount = amount
+
+    @property
+    def sell_to_buy_price(self) -> Wad:
+        return self.price
+
+    @property
+    def buy_to_sell_price(self) -> Wad:
+        return self.price
+
+    @property
+    def remaining_buy_amount(self) -> Wad:
+        return self.amount*self.price if self.is_sell else self.amount
+
+    @property
+    def remaining_sell_amount(self) -> Wad:
+        return self.amount if self.is_sell else self.amount*self.price
+
+    def __repr__(self):
+        return pformat(vars(self))
+
+    @staticmethod
+    def from_list(item: list, pair: str):
+        return Order(order_id=item['id'],
+                     pair=item['product_id'],
+                     is_sell=True if item['side'] == 'sell' else False,
+                     price=Wad.from_number(item['price']),
+                     amount=Wad.from_number(item['size']))
+
+
+class Trade:
+    def __init__(self,
+                 trade_id: Optional[id],
+                 timestamp: int,
+                 pair: str,
+                 is_sell: bool,
+                 price: Wad,
+                 amount: Wad):
+        assert(isinstance(trade_id, int) or (trade_id is None))
+        assert(isinstance(timestamp, int))
+        assert(isinstance(pair, str))
+        assert(isinstance(is_sell, bool))
+        assert(isinstance(price, Wad))
+        assert(isinstance(amount, Wad))
+
+        self.trade_id = trade_id
+        self.timestamp = timestamp
+        self.pair = pair
+        self.is_sell = is_sell
+        self.price = price
+        self.amount = amount
+
+    def __eq__(self, other):
+        assert(isinstance(other, Trade))
+        return self.trade_id == other.trade_id and \
+               self.timestamp == other.timestamp and \
+               self.pair == other.pair and \
+               self.is_sell == other.is_sell and \
+               self.price == other.price and \
+               self.amount == other.amount
+
+    def __hash__(self):
+        return hash((self.trade_id,
+                     self.timestamp,
+                     self.pair,
+                     self.is_sell,
+                     self.price,
+                     self.amount))
+
+    def __repr__(self):
+        return pformat(vars(self))
+
+    @staticmethod
+    def from_our_list(pair, trade):
+        return Trade(trade_id=trade['trade_id'],
+                     timestamp=int(dateutil.parser.parse(trade['created_at']).strftime("%s")),
+                     pair=pair,
+                     is_sell=True if trade['side'] == 'sell' else False,
+                     price=Wad.from_number(trade['price']),
+                     amount=Wad.from_number(trade['size']))
+
+    @staticmethod
+    def from_all_list(pair, trade):
+        return Trade(trade_id=trade['trade_id'],
+                     timestamp=int(dateutil.parser.parse(trade['time']).strftime("%s")),
+                     pair=pair,
+                     is_sell=True if trade['side'] == 'sell' else False,
+                     price=Wad.from_number(trade['price']),
+                     amount=Wad.from_number(trade['size']))
+
+
+class CoinbaseApi(PyexAPI):
+    """Coinbase API interface.
+    """
+
+    logger = logging.getLogger()
+
+    def __init__(self, api_server: str, api_key: str, secret_key: str, password: str, timeout: float):
+        assert(isinstance(api_key, str))
+        assert(isinstance(secret_key, str))
+        assert(isinstance(password, str))
+
+        self.api_server = api_server
+        self.api_key = api_key
+        self.secret_key = secret_key
+        self.password = password
+        self.timeout = timeout
+
+    def get_balances(self):
+        return self._http_authenticated("GET", "/accounts", {})
+
+    def get_balance(self, coin : str):
+        assert(isinstance(coin, str))
+        for balance in self.get_balances():
+            if balance['currency'] == coin:
+                return balance
+
+    def get_orders(self, pair: str) -> List[Order]:
+        assert(isinstance(pair, str))
+
+        orders = self._http_authenticated("GET", "/orders", {})
+
+        return list(map(lambda item: Order.from_list(item, pair), orders))
+
+    def place_order(self, pair: str, is_sell: bool, price: Wad, amount: Wad) -> str:
+        assert(isinstance(pair, str))
+        assert(isinstance(is_sell, bool))
+        assert(isinstance(price, Wad))
+        assert(isinstance(amount, Wad))
+
+        coins = pair.split("-")
+
+        data = {
+            "size": "%.8f" % float(amount),
+            "price": self._get_precision(coins[1]) % float(price),
+            "side": "sell" if is_sell else "buy",
+            "product_id": pair
+        }
+
+        self.logger.info(f"Placing order ({data['side']}, amount {data['size']} of {pair},"
+                         f" price {data['price']})...")
+
+        result = self._http_authenticated("POST", "/orders", data)
+        order_id = result['id']
+
+        self.logger.info(f"Placed order (#{result}) as #{order_id}")
+
+        return order_id
+
+    def cancel_order(self, order_id: str):
+        assert(isinstance(order_id, str))
+
+        self.logger.info(f"Cancelling order #{order_id}...")
+
+        return self._http_authenticated("DELETE", f"/orders/{order_id}", {})
+
+    def cancel_all_orders(self) -> List:
+        self.logger.info(f"Cancelling all orders ...")
+
+        result = self._http_authenticated("DELETE", "/orders", {})
+        success = len(result) > 0
+
+        if success:
+            self.logger.info(f"Cancelled orders : #{result}")
+        else:
+            self.logger.info(f"No order canceled ")
+
+        return result
+
+    def get_trades(self, pair: str) -> List[Trade]:
+        assert(isinstance(pair, str))
+
+        result = self._http_authenticated("GET", f"/fills?product_id={pair}", {})
+        print(result)
+
+        return list(map(lambda item: Trade.from_our_list(pair, item), result))
+
+    def get_all_trades(self, pair: str, page_number: int = 1) -> List[Trade]:
+        assert(isinstance(pair, str))
+        assert(isinstance(page_number, int))
+
+        limit = 100
+
+        result = self._http_unauthenticated("GET", f"/products/{pair}/trades?before={page_number}&limit={limit}", {})
+
+        return list(map(lambda item: Trade.from_all_list(pair, item), result))
+
+    def _http_authenticated(self, method: str, resource: str, body: dict):
+        assert(isinstance(method, str))
+        assert(isinstance(resource, str))
+        assert(isinstance(body, dict) or (body is None))
+
+        data = json.dumps(body, separators=(',', ':'))
+
+        timestamp = str(time.time())
+        message = ''.join([timestamp, method, resource, data or ''])
+        message = message.encode('ascii')
+        hmac_key = base64.b64decode(self.secret_key)
+        signature = hmac.new(hmac_key, message, hashlib.sha256)
+        signature_b64 = base64.b64encode(signature.digest()).decode('utf-8')
+
+        return self._result(requests.request(method=method,
+                                             url=f"{self.api_server}{resource}",
+                                             data=data,
+                                             headers={
+                                                 'Content-Type': 'Application/JSON',
+                                                 'CB-ACCESS-SIGN': signature_b64,
+                                                 'CB-ACCESS-TIMESTAMP': timestamp,
+                                                 'CB-ACCESS-KEY': self.api_key,
+                                                 'CB-ACCESS-PASSPHRASE': self.password
+                                             },
+                                             timeout=self.timeout))
+
+    def _http_unauthenticated(self, method: str, resource: str, body: dict):
+        assert(isinstance(method, str))
+        assert(isinstance(resource, str))
+        assert(isinstance(body, dict) or (body is None))
+
+        data = json.dumps(body, separators=(',', ':'))
+
+        return self._result(requests.request(method=method,
+                                             url=f"{self.api_server}{resource}",
+                                             data=data,
+                                             timeout=self.timeout))
+
+    def _result(self, result) -> Optional[dict]:
+        if not result.ok:
+            raise Exception(f"Coinbase API invalid HTTP response: {http_response_summary(result)}")
+
+        try:
+            data = result.json()
+        except Exception:
+            raise Exception(f"Coinbase API invalid JSON response: {http_response_summary(result)}")
+
+        return data
+
+    @staticmethod
+    def _get_precision(coin):
+        return {
+            'ETH': "%.7f",
+            'USDC': "%.2f"
+        }[coin]
+
+
+def http_response_summary(response) -> str:
+    text = response.text.replace('\r', '').replace('\n', '')[:2048]
+    return f"{response.status_code} {response.reason} ({text})"
