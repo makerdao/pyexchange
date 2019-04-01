06ea55180e7d2db22a05d8ab3d2bcfbdcafcb629
Ed Noepel <46749157+EdNoepel@users.noreply.github.com>
2019-04-01 01:17:35 -0400

    Upgrade OKEX API from v1 to v3 (#41)
    * Handled the low-lying fruit, added comments to more complex stuff
    
    * Got three market data methods working, and establishing a signing routine which works for HTTP GETS
    
    * POST request to cancel orders now working
    
    * Progress on trading functionality
    
    * Abandon cursor implementation, which seems broken on server side.  Add manual test code to check order collection.  Implement method to get our own trades using filtered orders.
    
    * Only send auth headers for requests which require them.
    
    * Discovered querying all trades doesn't actually require auth.  Now testing trades collection.
    
    * Fixed bug involving filled_amount, added unit test to exercise order class.
    
    * Correct balances API to point to /api/spot/v3/accounts.  Started work on unit test facility which mocks OKEX HTTP responses.
    
    * Implemented unit tests for all OKEX API methods.  Added status to order object for debugging purposes.  Properly handle non-integer order IDs when submitting and cancelling orders.
    
    * Moved order/trade test methods to test_okex module
    
    * Per code review, removed Order.status field, adjusted get_orders_history to omit filled orders, remove debugging statements, and submit get_trades as a single request.
    
    * Ignore another _virtualenv folder name, used in other projects
    
    * OKEX wasn't happy with urllib's encoding.
    
    * OKEX decided to correct the spelling of _cancelling_ status.

--- tests/manual_test_okex.py
+++ tests/manual_test_okex.py
@@ -1,6 +1,6 @@
 # This file is part of Maker Keeper Framework.
 #
-# Copyright (C) 2017-2018 reverendus
+# Copyright (C) 2017-2019 reverendus and EdNoepel
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
@@ -15,13 +15,66 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import datetime
 import sys
 
+from pymaker import Wad
 from pyexchange.okex import OKEXApi
 
 
-okex = OKEXApi('https://www.okex.com', sys.argv[1], sys.argv[2], 15.5)
+okex = OKEXApi(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], 15.5)
+print(sys.argv)
+print("OKEXApi created\n")
 
-print(okex.candles('eth_usdt', '1min', 75))
-# print(okex.get_balances())
-# print(len(okex.get_orders_history('mkr_eth', 1000)))
+
+pair = "mkr_usdt"
+# l1 = okex.ticker(pair)
+# print(f"best bid: {l1['best_bid']}  best ask: {l1['best_ask']}")
+# book = okex.depth(pair)
+# print(f"bids: {book['bids'][0:3]}")
+# print(f"asks: {book['asks'][0:3]}")
+# print(okex.candles(pair, '1min')[0:3])
+# print()
+
+balances = okex.get_balances()
+print(f"Account balances -- USDT: {balances['USDT']}")
+print(f"                     MKR: {balances['MKR']}")
+
+
+# price in terms of quote currency (USDT), size in terms of base currency (MKR)
+# print(okex.place_order(pair, False, Wad.from_number(699.33), Wad.from_number(0.25)))
+# print(okex.cancel_order(pair, "2502374107516928"))
+
+
+def print_orders(orders):
+    print(f"received {len(orders)} orders")
+    for index, order in enumerate(orders):
+        side = "sell" if order.is_sell else "buy "
+        fill_status = f"with {order.filled_amount} filled" if order.filled_amount > Wad(0) else "unfilled"
+        print(f"[{index}] {order.order_id} {side} {str(order.amount)[:9]} "
+              f"at {str(order.price)[:12]} "
+              f"on {datetime.datetime.utcfromtimestamp(order.timestamp)} "
+              + fill_status)
+        #f"page {order.page}")
+
+def print_trades(trades):
+    for trade in trades:
+        side = "sell" if trade.is_sell else "buy "
+        print(f"{side} {str(trade.amount)[:9]} {trade.amount_symbol} "
+              f"at {str(trade.price)[:12]} "
+              f"{pair.split('_')[1]} "
+              f"on {datetime.datetime.utcfromtimestamp(trade.timestamp)}")
+        
+
+# Gets open orders
+orders = okex.get_orders(pair)
+print_orders(orders)
+# Gets all orders
+orders = okex.get_orders_history(pair, 9)
+print_orders(orders)
+
+#trades = okex.get_trades(pair)
+#print(trades[:3])
+# trades = okex.get_all_trades(pair)
+# print_trades(trades[:9])
+
