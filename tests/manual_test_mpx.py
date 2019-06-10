cfacd5ddc731823bd099dcbe348f2478afaecbe4
grandizzy <grandizzy@localhost>
2019-06-10 10:32:37 +0300

    Get MPX token for each call

--- tests/manual_test_mpx.py
+++ tests/manual_test_mpx.py
@@ -35,7 +35,6 @@ zrx_exchange = ZrxExchangeV2(web3, EXCHANGE_ADDR)
 pair = MpxPair("WETH-DAI", WETH_ADDR, 18, DAI_ADDR, 18)
 api = MpxApi("https://api.mpexchange.io", zrx_exchange, FEE_RECIPIENT, 9.5, None)
 
-api.authenticate()
 print(api.get_markets())
 print(api.get_fee_recipients())
 
