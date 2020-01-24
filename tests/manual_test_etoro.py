14c2ab3588a951f0b9a612f04ee36b3712515cb4
Mike <mikehathaway@makerdao.com>
2020-01-24 12:42:02 -0800

    rename instrument_id to pair to conform to sync_trades interface

--- tests/manual_test_etoro.py
+++ tests/manual_test_etoro.py
@@ -1,6 +1,6 @@
 # This file is part of Maker Keeper Framework.
 #
-# Copyright (C) 2017-2018 reverendus
+# Copyright (C) 2020 reverendus
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
@@ -20,8 +20,9 @@ import sys
 from pyexchange.etoro import EToroApi
 from pymaker.numeric import Wad
 
-# etoro = EToroApi(sys.argv[1], 'test_account', sys.argv[2], sys.argv[3], 9.5)
-etoro = EToroApi('https://7vriaeqd.hft.etorox.com', 'test_account', sys.argv[1], open('./.etoro-unencrypted-key', 'r').read(), 9.5)
+# etoro = EToroApi(https://7vriaeqd.hft.etorox.com, 'test_account', sys.argv[2], sys.argv[3], 9.5)
+etoro = EToroApi('https://pk2kofdw.hft.etorox.com', 'mm@liquidityproviders.io', sys.argv[1], open('./.etoro-unencrypted-key', 'r').read(), 9.5)
+# etoro = EToroApi('https://7vriaeqd.hft.etorox.com', 'mm@liquidityproviders.io', sys.argv[1], open('./.etoro-unencrypted-key', 'r').read(), 9.5)
 
 print("Starting eToroAPI with the following parameters: ", sys.argv)
 
@@ -32,18 +33,23 @@ print("Starting eToroAPI with the following parameters: ", sys.argv)
 # GET "/api/v1/balances"
 # print(etoro.get_balances())
 
+# GET "/api/v1/order/{oid}"
+# 29029673-3ac3-4b89-9fdb-df2c30744555
+# print(etoro.get_order("57d5e7f0-2b4e-42cf-b544-800d97301f5a"))
+
 # GET "/api/v1/orders"
-# print(etoro.get_orders('ethusdc', '1', 'open', 25))
+print(etoro.get_orders('ethusdc', "open"))
+# print(etoro.get_orders('ethusdc', 'open', '1', 25))
 
 # POST /api/v1/orders
-# print(etoro.place_order('ethusdc', 'buy', Wad.from_number(144.94033), Wad.from_number(.005)))
+# print(etoro.place_order('ethusdc', 'buy', Wad.from_number(167.94033), Wad.from_number(.005)))
 
 # DELETE /api/v1/orders/{order_id}
 # print(etoro.cancel_order('c8e579b0-cfb8-4297-9983-7deb5c454761'))
 
 # GET /api/v1/trades
-print(etoro.get_trades('ethusdc'))
-# print(etoro.get_trades('ethusdc', '1578959828'))
+# print(etoro.get_trades('ethusdc'))
+# print(etoro.get_trades('ethusdc', '15789598280'))
 
 # GET /api​/v1​/funds​/deposits​/{coin}​/address
 # print(etoro.get_deposit_address('eth'))
