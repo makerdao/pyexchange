ec5c65b50a4ad90b8ef9716fc970bf0e20de565a
Mike Hathaway <mahathaway93@gmail.com>
2020-02-14 13:21:29 -0800

    Add bitso (#116)
    * add bitso pyex api client
    
    * add testing files
    
    * update tests
    
    * replace instrument_id with book
    
    * update bitso pyexchange to support keeper and sync trades
    
    * add tests; fix query param bug
    
    * add remaining unit tests
    
    * fix test running
    
    * remove unused import
    
    * replace base Exception in Bitso with ValueError

---
+++ tests/manual_test_bitso.py
@@ -0,0 +1,46 @@
+# This file is part of Maker Keeper Framework.
+#
+# Copyright (C) 2020 MakerDAO
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import sys
+
+from pyexchange.bitso import BitsoApi
+from pymaker.numeric import Wad
+
+bitso = BitsoApi('https://api.bitso.com', sys.argv[1],  sys.argv[2], 9.5)
+print("Starting BitsoApi with the following parameters: ", sys.argv)
+
+# GET "/v3/balance/"
+# print(bitso.get_balances())
+
+#print(bitso.get_markets())
+# print(bitso.get_pair('ETH/USDC'))
+
+# GET "/api/v1/orders"
+# print(bitso.get_orders('eth_mxn'))
+
+# POST /api/v1/orders
+# print(bitso.place_order('eth_mxn', 'buy', 5200.000, .01))
+
+# DELETE /api/v1/orders/{order_id}
+print(bitso.cancel_order('w0Z3B1MmmcytBFu0'))
+
+# GET /api/v1/trades
+# print(bitso.get_trades("eth_mxn"))
+# print(bitso.get_trades('ethusdc', '1578959828'))
+
+# GET /api​/v1​/funds​/deposits​/{coin}​/address
+# print(bitso.get_deposit_address('eth'))
