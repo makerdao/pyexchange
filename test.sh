24f5fca00b01db0393110acf626462cbe9b99636
grandizzy <grandizzy@localhost>
2020-05-21 09:09:17 +0300

    Report test failures

--- test.sh
+++ test.sh
@@ -4,6 +4,9 @@
 py-testrpc -p 8889 &
 
 PYTHONPATH=$PYTHONPATH:./lib/pymaker py.test -x --cov=pyexchange --cov-report=term --cov-append tests/
+TEST_RESULT=$?
 
 # kill the local server upon completion of tests
 pid=$(lsof -i:8889 -t); kill -TERM $pid || kill -KILL $pid
+
+exit $TEST_RESULT
