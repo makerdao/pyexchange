1f4cd39295927cee95b21d74377a62ccb64f81f4
Mike <mikehathaway@makerdao.com>
2020-06-17 14:50:11 -0700

    fix okex check_result to account for stringified code

--- pyexchange/okex.py
+++ pyexchange/okex.py
@@ -348,7 +348,7 @@ class OKEXApi:
         # file.close()
 
         if check_result:
-            if 'error_code' in data and data["error_code"] != 0 and data["error_code"] != "":
+            if 'error_code' in data and data["error_code"] != "0" and data["error_code"] != "":
                 raise Exception(f"OKCoin API negative response: {http_response_summary(response)}")
 
         if has_cursor:
