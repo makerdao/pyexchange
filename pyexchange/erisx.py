e8e8009038c79bb5a60e53692ee2eb7dff3519ab
Mike <mikehathaway@makerdao.com>
2020-12-05 18:24:48 -0500

    handle eridx orders that have been filled before they can be cancelled

--- pyexchange/erisx.py
+++ pyexchange/erisx.py
@@ -315,6 +315,9 @@ class ErisxApi(PyexAPI):
         if response.get(150) is not None:
             if response.get(150).decode('utf-8') == '4':
                 return True, False
+            elif response.get(150).decode('utf-8') == 'F':
+                self.logger.warning(f"Order was filled before it could be cancelled: {response.get(simplefix.TAG_ORDERID)}|{response.get(simplefix.TAG_CLORDID)}")
+                return False, True
             else:
                 self.logger.warning(f"Order not cancelled: {response.get(simplefix.TAG_ORDERID)}|{response.get(simplefix.TAG_CLORDID)}")
                 return False, False
