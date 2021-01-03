22e5d50aebfd7547942d0aecb684b7466fbbd9a3
Ian Harvey <ith.harvey@gmail.com>
2021-01-02 21:47:51 -0500

    Failed attempt to re-create nonce error

--- tests/manual_test_liquid.py
+++ tests/manual_test_liquid.py
@@ -16,21 +16,74 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import sys
+import threading
 
+from concurrent.futures import ThreadPoolExecutor
 from pyexchange.liquid import LiquidApi
 from pymaker import Wad
 
-liquid = LiquidApi('https://api.quoine.com', sys.argv[1], sys.argv[2], 9.5)
+
+liquid = LiquidApi("https://api.liquid.com", sys.argv[1], sys.argv[2], 9.5)
+_executor = ThreadPoolExecutor(max_workers=5)
+
+data_set = [
+        {
+            'sell_order_bool': True,
+            'price': Wad.from_number(29002),
+            'amount': Wad.from_number(0.01)
+        },
+        {
+            'sell_order_bool': True,
+            'price': Wad.from_number(29001),
+            'amount': Wad.from_number(0.01)
+        },
+        {
+            'sell_order_bool': True,
+            'price': Wad.from_number(29000),
+            'amount': Wad.from_number(0.01)
+        },
+        {
+            'sell_order_bool': False,
+            'price': Wad.from_number(28000),
+            'amount': Wad.from_number(0.01)
+        },
+        {
+            'sell_order_bool': False,
+            'price': Wad.from_number(28001),
+            'amount': Wad.from_number(0.01)
+        },
+        {
+            'sell_order_bool': False,
+            'price': Wad.from_number(28002),
+            'amount': Wad.from_number(0.01)
+        },
+        {
+            'sell_order_bool': False,
+            'price': Wad.from_number(28003),
+            'amount': Wad.from_number(0.01)
+        }
+        ]
+
+for data in data_set:
+    _executor.submit(liquid.place_order("BTCDAI", data['sell_order_bool'], data['price'], data['amount']))
+
 
 # print(liquid.get_markets())
 # print(liquid.get_pair("ETHUSDC"))
 print(liquid.get_balances())
-order = liquid.place_order("ETHUSDC", False, Wad.from_number(260), Wad.from_number(0.01))
-# print(order)
-print(liquid.get_orders("ETHUSDC"))
+
+
+print("tasks should be complete")
+orders = liquid.get_orders("BTCDAI")
+print(f"orders {orders}")
+
+for order in orders:
+    liquid.cancel_order(str(order.order_id))
+
 # print(liquid.get_trades("ETHUSDC"))
 # print(liquid.get_balances())
-print(liquid.cancel_order(str(order)))
-print(liquid.get_orders("ETHUSDC"))
+#print(liquid.cancel_order(str(order_a)))
+#print(liquid.cancel_order(str(order_b)))
+print(liquid.get_orders("BTCDAI"))
 # print(liquid.get_orders("ETHUSDC"))
 # print(liquid.get_all_trades("VETETH"))
